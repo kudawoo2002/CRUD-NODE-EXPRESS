7bab02d715d2cec0952be815b148010081681e82
Kwami Kudawoo <kudawoo2002@gmail.com>
2025-01-27 21:16:30 +0000

    add env file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+import "dotenv/config";
 import express from "express";
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 let teas = [];
